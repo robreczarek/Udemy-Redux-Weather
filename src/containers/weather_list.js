5887153c1c4d375397103eea88aeb53e47144471
Robert Obreczarek <robert@obreczarek.com>
2017-03-31 15:42:00 +0200

    Completed Section 5, Lecture 64

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
+  
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    const temps = cityData.list.map(temperature => temperature.main.temp);
+    const pressure = cityData.list.map(pressure => pressure.main.pressure);
+    const humidity = cityData.list.map(humidity => humidity.main.humidity);
+
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+        <td><Chart data={temps} color="orange" /></td>
+        <td><Chart data={pressure} color="green" /></td>
+        <td><Chart data={humidity} color="black" /></td>
+      </tr>
+    );
+  }
+
   render() {
     return (
       <table className="table table-hover">
@@ -14,12 +32,7 @@ class WeatherList extends Component {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     );
